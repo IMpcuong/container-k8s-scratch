0c29930b4c6d4742f070ef48722f0c3377092e68
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-07-03 10:19:06 +0700

    fixbug: Stuck in detects which shell in use Windows (pwsh)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 // docker         | run <image_name> | <cmd> <args> (equals with:)
@@ -20,12 +21,28 @@ func main() {
 }
 
 func run() {
+	var cmd *exec.Cmd
+
 	// os.Args[2:] := is the command line with all the arguments we expect to run inside the container.
 	fmt.Printf("Running cmd %v\n", os.Args[2:])
 
 	// os.Args[2]    := "echo"
-	// os.Args[3]... := "hello The Office"
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	// os.Args[3]... := "-n 'hello The Office'"
+	if runtime.GOOS == "windows" {
+		fmt.Printf("Detecting OS...: %s\n", runtime.GOOS)
+
+		// windowsCmd := "cmd /c " + os.Args[2]
+		// cmd = exec.Command(windowsCmd, os.Args[3:]...)
+		windowsCmd := "powershell -NoLogo -NoProfile -NonInteractive " + os.Args[2]
+		cmd = exec.Command(windowsCmd, os.Args[3:]...)
+		execCmd(cmd)
+	}
+
+	cmd = exec.Command(os.Args[2], os.Args[3:]...)
+	execCmd(cmd)
+}
+
+func execCmd(cmd *exec.Cmd) {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr

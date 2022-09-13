8ed9e7c25b406679b1b3cd4436c66599f3e515d0
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-09-13 16:01:04 +0700

    script: `docker` run in interactive mode with correct image tag

--- docker-git/run.bat
+++ docker-git/run.bat
@@ -2,4 +2,4 @@
 
 @REM This new created image only contains `git`.
 @REM And the entrypoint configures the container's mount-point that is exposing to be executable by the users.
-docker container run --rm -it docker-git-test:auto status
+docker container run --rm -it impcuong/docker-git-test:latest status
